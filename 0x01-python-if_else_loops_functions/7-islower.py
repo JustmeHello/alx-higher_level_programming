bf1e26ec9ceced90409de60279b0906b66b862e8
JustmeHello <aribat@proton.me>
2023-11-28 07:17:03 -0800

    up

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    return ord('a')<=ord(c)<=ord('z')
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            uppercase_char = chr(ord(char) - 32)
+            print(uppercase_char, end="")
+        else:
+            print(char, end="")
+    print()
