512ca1d6db75799073a31fc62dc5ac56fca027f8
JustMe <119158894+JustmeHello@users.noreply.github.com>
2023-11-29 11:23:41 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(90, 64, -1)
-    print("{:c}.format(i), end="" if i%2==0 else "{:c}".format(i+32))
-print()
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
