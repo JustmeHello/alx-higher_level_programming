ba95a1fca792229c3792a952912dc782ec71e3ae
JustmeHello <aribat@proton.me>
2023-11-30 11:26:56 -0800

    up

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(a, b):
+    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+    
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
