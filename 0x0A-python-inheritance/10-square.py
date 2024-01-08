2654f04a8746bf4a29908d3ce9514bd42ea07c86
JustMe <119158894+JustmeHello@users.noreply.github.com>
2024-01-08 19:29:24 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,22 @@
+#!/usr/bin/python3
+Write a class Square that inherits from Rectangle (9-rectangle.py):
+
+    Instantiation with size: def __init__(self, size)::
+        size must be private. No getter or setter
+        size must be a positive integer, validated by integer_validator
+    the area() method must be implemented
+
+guillaume@ubuntu:~/0x0A$ cat 10-main.py
+#!/usr/bin/python3
+Square = __import__('10-square').Square
+
+s = Square(13)
+
+print(s)
+print(s.area())
+
+guillaume@ubuntu:~/0x0A$ ./10-main.py
+[Rectangle] 13/13
+169
+guillaume@ubuntu:~/0x0A$ 
 
