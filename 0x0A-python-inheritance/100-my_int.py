b5de621b43bb058764ed7a77823732672bcd1049
JustMe <119158894+JustmeHello@users.noreply.github.com>
2024-01-08 19:32:01 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """Class inheriting from int with inverted == and != operators."""
 
+    def __eq__(self, other):
+        """Inverts the behavior of the == operator."""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Inverts the behavior of the != operator."""
+        return super().__eq__(other)
